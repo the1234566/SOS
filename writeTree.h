5de5d68237b8442aaf59ca102be0f1152ccc1404
the1234566 <45755623+the1234566@users.noreply.github.com>
2018-12-14 09:34:50 +0800

    Delete writeTree.h

--- writeTree.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "treeStructure.h"
-#ifndef writeTree_H
-#define writeTree_H
-void writeTree( Node *head );
-void writeNode( FILE *fp, Node *node );
-void printOut( FILE *fp, Node *node );
-#endif
